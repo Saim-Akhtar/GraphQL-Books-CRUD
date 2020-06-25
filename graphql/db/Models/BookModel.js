13726d04d62cde1dc5aceae5dfdb99095d9e3215
Saim Akhtar <saimakhtar98@gmail.com>
2020-06-25 21:03:07 +0500

    created BookModel

---
+++ graphql/db/Models/BookModel.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose')
+
+const BookSchema = Schema({
+    _id: Schema.Types.ObjectId,
+    title: { type: String, required: true },
+    authorId: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+})
+
+module.exports = model('Book',BookSchema)
